14753e4b5dcedb447d6ce8e894fdd9309f4464fd
NISAMLC <61561337+NISAMLC@users.noreply.github.com>
2020-06-18 22:05:44 +0530

    Add files via upload

---
+++ Assignment june14.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Assignment-1
+
+# In[5]:
+
+
+x=[]
+for i in range(2000,3201):
+    if i%7 ==0 and i%5 != 0:
+        x.append(i)
+
+
+# In[6]:
+
+
+print(x)
+
+
+# In[ ]:
+
+
+
+
+
+# # Assignment-2
+
+# In[8]:
+
+
+first_name = input("enter your first name ")
+last_name =input("enter your last name ")
+print(first_name, '' +  last_name)
+
+
+# # Assignment-3
+
+# In[16]:
+
+
+diameter =12
+r = float (diameter //2)
+v = (4/3) * 3.14 *r**3
+print(v)
+
+
+# In[ ]:
+
+
+
+
